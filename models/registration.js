d07e416c0b973a9e260a18738a3fff098dea111c
supriyo522 <134998523+supriyo522@users.noreply.github.com>
2024-09-11 16:42:32 +0530

    Update registration.js

--- models/registration.js
+++ models/registration.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
 const registrationSchema = new mongoose.Schema({
-    name: String,
-    mobileNo: String,
-    email: String,
-    billNumber: String
+    name: { type: String, required: true },
+    mobileNo: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    billNumber: { type: String, required: true, unique: true }
 });
+
+// Create unique index to ensure uniqueness in the database
+registrationSchema.index({ mobileNo: 1, email: 1, billNumber: 1 }, { unique: true });
 
 const Registration = mongoose.model('Registration', registrationSchema);
 
 module.exports = Registration;
-
